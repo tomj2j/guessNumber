9f80989f379c9b9337f724362df505c3de93477d
Thomas Werner <tom.renrew@gmail.com>
2022-06-13 19:20:49 +0200

    added function and gamelogic

--- script.js
+++ script.js
@@ -13,12 +13,40 @@ const secretNumber = Math.trunc(Math.random() * 20 + 1);
 console.log(secretNumber);
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   console.log(guess, typeof guess);
 
+  const lostGame = function () {
+    document.querySelector('.message').textContent = ' 🧨🤯 You lost!';
+    document.querySelector('.score').textContent = 0;
+  };
+
+  const lowerPrintScore = function () {
+    score--;
+    document.querySelector('.score').textContent = score;
+  };
+
   if (!guess) {
     document.querySelector('.message').textContent = ' 🐻‍❄️ No Number entered';
-  } else if (guess === secretNumber) document.querySelector('.message').textContent = 'CORRECT !! 🎉';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'CORRECT !! 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' 🛫 Too high!';
+      lowerPrintScore();
+    } else {
+      lostGame();
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🛬 Too low!';
+      lowerPrintScore();
+    } else {
+      lostGame();
+    }
+  }
 });
